24acdeafd03021eab9cc9a7a4db0465d50525321
takhat <takhat014@gmail.com>
2022-12-17 15:37:52 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Bike from "./components/Bike";
+
+function App() {
+  return (
+    <div className="App">
+      <Bike />
+      <button>Add Bike</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Bike.js
@@ -0,0 +1,22 @@
+const Bike = () => {
+  const myBike = {
+    id: 1,
+    name: "My Bike",
+    size: 50,
+    price: 100,
+    type: "special",
+  };
+  return (
+    <div>
+      <h2>{myBike.name}</h2>
+      <ul>
+        <li>{myBike.id}</li>
+        <li>{myBike.size}</li>
+        <li>{myBike.price}</li>
+        <li>{myBike.type}</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Bike;
